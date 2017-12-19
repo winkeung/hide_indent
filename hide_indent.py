149da15508f6f3cac53563894e35ccc78b35aa18
winkeung <winkeung@gmail.com>
2017-12-20 00:30:09 +0800

    implemented hide_selection(), but only work for uno, not COM

--- hide_indent.py
+++ hide_indent.py
@@ -174,15 +174,15 @@ def group_recursive(
     # row_indent = last_indent_cell
     return last_row, last_indent_cell, last_indent_char, blank_row_cnt
 
-# TODO: merge group() and group_cell_indent() into 1 function
-#
-# selection area:     | indentation unit | range 
-#---------------------+------------------+--------------------------------------------
-# 1 row, 1 column     | character        | from selected row to whole sheet
-# 1 row, >1 columns   | cell             | from selected top left cell to whole sheet
-# >1 row, 1 columns   | character        | selected rows
-# >1 row, >1 columns  | cell             | selected range 
+
 def group_selection():
+    """Group the selected rows according to their indentations.
+        # selection area: | range
+        # -----------------+-----------------------------------
+        # 1 row           | from selected row to whole sheet
+        # >1 row          | selected rows
+    """
+
     #get the first sheet of the spreadsheet doc
     #xSheet = ThisComponent.Sheets(iCurSheet)
     # xSheet=thiscomponent.getcurrentcontroller.activesheet
@@ -216,7 +216,7 @@ def group_selection():
     global level
     level = 0
 
-    blank_row_cnt = 0 #any value will do, this ByRef parameter serve as output only
+    # blank_row_cnt = 0 #any value will do, this ByRef parameter serve as output only
     
     # if addr.StartColumn == addr.EndColumn : #single column is selected, char indentation mode
     #     row_indent = findNoIndentChar(xSheet.getCellByPosition(col, row).getString())
@@ -256,7 +256,152 @@ def group_selection():
             end_row)
         if indent_cell < 0:
             break
-        print "here"
+        # print "here"
+
+def select(scol, srow, lcol, lrow):
+    #'dim oSheet, oRange, oCell, oController
+    model = desktop.getCurrentComponent()
+    oController = model.getCurrentController()
+    #oSheet = model.sheets(1)
+    #oSheet = model.Sheets.getByIndex(0)  # access the active sheet
+    oSheet = model.CurrentController.ActiveSheet
+    #oRange = oSheet.getCellRangeByname("B2:D3")
+    oRange = oSheet.getCellRangeByPosition(scol, srow, lcol, lrow)
+    oController.select(oRange)
+
+def set_selection_visible(isVisible):
+    model = desktop.getCurrentComponent()
+    document = model.getCurrentController()
+    dispatcher = smgr.createInstance( "com.sun.star.frame.DispatchHelper")
+
+    struct = get_struct()
+
+    struct.Name = 'RowOrCol'
+    struct.Value = 'R'
+
+    if isVisible:
+        cmd_str = ".uno:ShowRow"
+    else:
+        cmd_str = ".uno:HideRow"
+
+    dispatcher.executeDispatch(document, cmd_str, "", 0, tuple([struct]))
+
+def set_rows_visible(start_row, no_of_row, isVisible):
+    doc = desktop.getCurrentComponent()
+    sheet = doc.CurrentController.getActiveSheet()
+
+    # Col = sheet.Columns[1]
+    Rows = sheet.Rows
+    for r in range(start_row, start_row + no_of_row):
+        try:
+            Rows[r].IsVisible = isVisible
+            # print("IsVisible")
+        except:
+            # backup current selection
+            oSelection = doc.getCurrentSelection()
+            oArea = oSelection.getRangeAddress()
+            frow = oArea.StartRow
+            lrow = oArea.EndRow
+            fcol = oArea.StartColumn
+            lcol = oArea.EndColumn
+
+            select(fcol, start_row, fcol, start_row + no_of_row - 1)
+            set_selection_visible(isVisible)
+            # print("set_selection_visiable")
+
+            # restore previous selection
+            select(fcol, frow, lcol, lrow)
+            break
+
+def check_row_visible(r):
+    doc = desktop.getCurrentComponent()
+    sheet = doc.CurrentController.getActiveSheet()
+
+    # Col = sheet.Columns[1]
+    Row = sheet.Rows[r]
+    # sheet.Rows.hideByIndex(i,1)
+    return Row.IsVisible
+
+def hide_selection():
+    """Cycle between collapse all, expand one level, and expand all (treat indentation as indicator of tree relationship).
+        Walk thru all rows and expand all immediate children if not yet expanded and check whether it is originally
+        all expanded or all collapsed. And then do the following actions:
+
+        all already collapsed:     expand 1 level (already done)
+        all already expanded:      collapse all
+        none of the above:         expand all
+    """
+
+    doc = desktop.getCurrentComponent()
+    global oController
+    oController = doc.CurrentController
+    global xSheet
+    xSheet = doc.CurrentController.getActiveSheet()
+
+    oSel = doc.getCurrentSelection()  # or oView.getSelection()
+    addr = oSel.getRangeAddress()
+
+    # print addr.StartRow, addr.EndRow, addr.StartColumn, addr.EndColumn
+    # dim col as long, row as long, row_indent as long, end_col as long, end_row as long
+    col = addr.StartColumn
+    row = addr.StartRow
+
+    c = xSheet.createCursor()
+    c.gotoEndOfUsedArea(False)
+
+    end_row = c.RangeAddress.EndRow
+    end_col = c.RangeAddress.EndColumn
+
+    indent_cell = findNoIndentCell(col, end_col, row)
+    indent_char = findNoIndentChar(xSheet.getCellByPosition(col + indent_cell, row).getString())
+
+    isAlreadyAllExpanded = True
+    isAlreadyAllCollapsed = True
+    last_row = row
+    child_indent_cell = 1025 # lastest encountered immediate child's indentation, init to a impossible big number
+    child_indent_char = 0
+    blank_row_cnt = 0 # no. blank row above the current visiting row
+    while True:
+        last_row += 1
+        if end_row < last_row:
+            break
+
+        last_indent_cell = findNoIndentCell(col, end_col, last_row)
+        last_indent_char = 0
+
+        if 0 <= last_indent_cell:
+            last_indent_char = findNoIndentChar(xSheet.getCellByPosition(col + last_indent_cell, last_row).getString())
+            if 0 <= last_indent_char: # not blank row
+                if indent_cell < last_indent_cell or (
+                    (indent_cell == last_indent_cell) and (indent_char < last_indent_char)):  # next item is deeper indented
+                    if check_row_visible(last_row):
+                        # print ("here")
+                        isAlreadyAllCollapsed = False
+                    else:
+                        # print ("there")
+                        isAlreadyAllExpanded = False
+                    if child_indent_cell < last_indent_cell or (
+                                (child_indent_cell == last_indent_cell) and (
+                                child_indent_char < last_indent_char)):  # next item is deeper indented then lastest encountered immediate child
+                        pass
+                    else: # encounter a new immediate child
+                        child_indent_cell = last_indent_cell
+                        child_indent_char = last_indent_char
+                        set_rows_visible(last_row - blank_row_cnt, blank_row_cnt + 1, True)
+                else:
+                    break # this row is not deeper indented
+                blank_row_cnt = 0
+            else:
+                blank_row_cnt += 1 # blank row
+        else:
+            blank_row_cnt += 1 # blank row
+
+    if isAlreadyAllExpanded:
+        # collapse all
+        set_rows_visible(row + 1, last_row - row - 1 - blank_row_cnt, False)
+    elif not isAlreadyAllCollapsed:
+        # expand all
+        set_rows_visible(row + 1, last_row - row - 1 - blank_row_cnt, True)
 
 if __name__ == "__main__":
     group_selection()
